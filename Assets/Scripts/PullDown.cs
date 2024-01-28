005f2f1e1f12216160934b82fb23599ce3994668
choosecake <imchoosecake@gmail.com>
2024-01-28 10:54:22 -0300

    fixes colliders

---
+++ Assets/Scripts/PullDown.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullDown : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.up, ForceMode.VelocityChange);
+    }
+}
